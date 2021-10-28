a03dfb874ae5524a1e55571f26f33e6d00ec6370
seal144 <rafalfak8@gmail.com>
2021-10-28 07:43:28 +0200

    feat: handle addInput

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,36 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
 
 import { StoreContext } from '../store/StoreProvider';
 
+let lastId = 0;
+
 const Main = () => {
   const { currencies, gramOfGoldValue, cryptoCurrencies } = useContext(StoreContext);
+  const [ inputFilds, setInputFields ] = useState([]);
+  const [ mainUnit, setMainUnit ] = useState('PLN');
+  const [ sum, setSum ] = useState(0);
+
+  const addField = () => {
+    const newField = {
+      id: lastId + 1,
+      value: 0,
+      unit: mainUnit
+    }
+    lastId++;
+    setInputFields(prevState => [...prevState, newField])
+  }
 
   console.log(currencies, gramOfGoldValue, cryptoCurrencies);
 
+  const displayInputFields = inputFilds.map(item => <p>{item.id}</p>)
+
   return (
-    <>
-      <h1>MAIN</h1>
-    </>
+    <div className="main">
+      <h1 className="main__title">MAIN</h1>
+      {displayInputFields}
+      <button onClick={addField}><PlusOutlined /></button>
+    </div>
   )
 }
 
