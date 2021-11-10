917e5a3b8f4aec4dde40f389ec619fd82a5e40ef
seal144 <rafalfak8@gmail.com>
2021-11-10 14:28:40 +0100

    rework: home add description of app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <>
-      <h1>HOME PAGE</h1>
+    <div className="home">
+      <h2 className="home__title">ABOUT</h2>
+      <p className="home__paragrah">
+        This is "YOUR_WEALTH" - simple web app, done for training purposes. It was created using React.js.
+        App is fetching data from public API ( api.nbp.pl and api.bitbay.net) about currencies, crypto currencies and gold rate. You can input assets in various units and app will calculate for you sum of this assets in unit of your choice using current rate.
+      </p>
       <Link className="button" to='/main'>START</Link>
-    </>
+    </div>
   )
 }
 
