870409707be28278e984aa2f833d000efe279885
seal144 <rafalfak8@gmail.com>
2021-11-17 20:32:57 +0100

    fix: typo on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <h2 className="home__title">ABOUT</h2>
       <p className="home__paragraph">
         This is "YOUR_WEALTH" - simple web app, done for training purposes. It was created using React.js.
-        App is fetching data from public API ( api.nbp.pl and api.bitbay.net) about currencies, crypto currencies and gold rate. You can input assets in various units and app will calculate for you sum of this assets in unit of your choice using current rate.
+        App is fetching data from public API ( api.nbp.pl and api.bitbay.net) about currencies, cryptocurrencies and gold rate. You can input assets in various units and app will calculate for you sum of this assets in unit of your choice using current rate.
       </p>
       <Link className="button home__button" to='/main'>START</Link>
     </div>
